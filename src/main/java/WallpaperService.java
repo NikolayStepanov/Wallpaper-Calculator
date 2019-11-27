d11bc3460be8badc9aac344acd6a93a79b26519f
Nikolay Stepanov <stepanovnikola96@mail.ru>
2019-11-27 14:02:17 +0300

    add calculete quantity canvases

--- src/main/java/WallpaperService.java
+++ src/main/java/WallpaperService.java
@@ -1,6 +1,12 @@
 public class WallpaperService {
-    public int calculatePerimeter(Room room)
-    {
-        return (room.getWidth()+room.getLength())*2;
+    public int calculatePerimeter(Room room) {
+        return (room.getWidth() + room.getLength()) * 2;
     }
+
+    public int getQuantityCanvases(int perimeterRoom, int widthWallpaper) {
+        int result = (int) Math.ceil(perimeterRoom / (double) widthWallpaper);
+        return result;
+    }
+
+
 }
--- src/test/java/WallpaperServiceTest.java
+++ src/test/java/WallpaperServiceTest.java
@@ -6,8 +6,20 @@ class WallpaperServiceTest {
 
     @Test
     void calculatePerimeter() {
-        Room room = new Room(700,800,300);
+        Room room = new Room(400, 500, 260);
         WallpaperService service = new WallpaperService();
-        assertEquals(3000,service.calculatePerimeter(room));
+        assertEquals(1800, service.calculatePerimeter(room));
+    }
+
+    @Test
+    void getQuantityCanvases() {
+        Room room = new Room(400, 500, 260);
+        Wallpaper wallpaper = new Wallpaper(53, 1005, 64);
+        WallpaperService service = new WallpaperService();
+
+        int perimeterRoom = service.calculatePerimeter(room);
+        int quantityCanvases = service.getQuantityCanvases(perimeterRoom, wallpaper.getWidth());
+
+        assertEquals(34, quantityCanvases);
     }
 }
